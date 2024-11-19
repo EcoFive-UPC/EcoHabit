7f4cec3989fc77c916730b326d160583801f541f
Luis910-vpn <luisdiegocamusdominguez@gmail.com>
2024-11-19 16:01:36 -0500

    index.html

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -49,3 +49,41 @@ descarga.addEventListener("click", (s) => {
     sectionD.scrollIntoView({behavior:"smooth"});
 })
 
+/*Register y Login*/
+var registroModal = document.getElementById("registro_modal");
+var loginModal = document.getElementById("login_contenedor");
+
+var registroBtn = document.getElementById("registrarse");
+var loginLink = document.getElementById("iniciar_sesion");
+
+var cerrarRegistro = document.getElementsByClassName("cerrar")[0];
+var cerrarLogin = document.getElementsByClassName("cerrar_login")[0];
+
+registroBtn.onclick = function () {
+    registroModal.classList.add("active");
+};
+
+loginLink.onclick = function (event) {
+    event.preventDefault(); // Prevenir la navegación del enlace
+    registroModal.classList.remove("active"); // Ocultar el modal de registro
+    loginModal.classList.add("active"); // Mostrar el modal de login
+};
+
+cerrarRegistro.onclick = function () {
+    registroModal.classList.remove("active");
+};
+
+cerrarLogin.onclick = function () {
+    loginModal.classList.remove("active");
+};
+
+window.onclick = function (event) {
+    if (event.target == registroModal) {
+        registroModal.classList.remove("active");
+    } else if (event.target == loginModal) {
+        loginModal.classList.remove("active");
+    }
+};
+
+
+
